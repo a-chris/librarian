d343a70d659b37c67d30b198b5b3f73d0fbe94af
a-chris <32082962+a-chris@users.noreply.github.com>
2022-07-04 00:06:37 +0200

    downcase extension and adds more types

--- script.rb
+++ script.rb
@@ -4,10 +4,11 @@ DIRS = {
   'Apps' => %w[dmg exe],
   'Books' => %w[epub],
   'Documents' => %w[pdf html doc docx],
-  'Data' => %w[xls xlsx csv md json],
+  'Data' => %w[xls xlsx csv md json xml],
   'Videos' => %w[mp4 flv mkv avi],
-  'Images' => %w[png jpg jpeg webp bmp],
-  'Archives' => %w[zip tg gz rar 7z]
+  'Images' => %w[png jpg jpeg webp bmp svg ico],
+  'Archives' => %w[zip tg gz rar 7z],
+  'Audios' => %w[m3u mp3 aac]
 }.freeze
 
 current_dir = Dir.new('.')
@@ -15,6 +16,6 @@ DIRS.each do |k, v|
   Dir.mkdir(k) unless File.exist?(k)
   current_dir.children.each do |file|
     # .filter { |f| v.any? { |ext| f.end_with?(ext) } }
-    FileUtils.mv(file, k) if v.include?(File.extname(file).tr('.', ''))
+    FileUtils.mv(file, k) if v.include?(File.extname(file).tr('.', '').downcase)
   end
 end
